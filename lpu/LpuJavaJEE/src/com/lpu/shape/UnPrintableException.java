35816c2198ea997666f8cd10a7a25f867e395d9b
Gyanendra Singh <gyanendra.singh@congnizant.com>
2023-01-18 11:54:20 +0530

    Added print exception to shape

--- lpu/LpuJavaJEE/src/com/lpu/shape/UnPrintableException.java
+++ lpu/LpuJavaJEE/src/com/lpu/shape/UnPrintableException.java
@@ -1,5 +1,14 @@
 package com.lpu.shape;
-
+/*
+ * There would be exceptional logic 
+ * to handle that you have to create a Exception class by 
+ * extending Exception.
+ * Then you have to throw where that exceptional logic comes
+ * in the method.
+ * All methods who are throwing any exception
+ * they have to declare it.
+ * OR they have to catch it.
+ */
 public class UnPrintableException extends Exception {
 	UnPrintableException(String message){
 		super(message);
