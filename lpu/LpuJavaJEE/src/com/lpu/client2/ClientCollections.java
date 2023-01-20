961fa360b8d39044009c1ffe05f1b8374bfaa1d8
Gyanendra Singh <gyanendra.singh@datacrux.com>
2023-01-20 13:03:13 +0530

    added set and ques

--- lpu/LpuJavaJEE/src/com/lpu/client2/ClientCollections.java
+++ lpu/LpuJavaJEE/src/com/lpu/client2/ClientCollections.java
@@ -2,7 +2,11 @@ package com.lpu.client2;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import com.lpu.domain.Employee;
 
@@ -20,15 +24,47 @@ public class ClientCollections {
 	
 
 	public static void main(String[] args) {
-		List<Employee> studList = new ArrayList<Employee>();
-	studList.add(new Employee("Emp1",1,10));
-	studList.add(new Employee("Emp2",2,20));
-	studList.add(new Employee("Emp3",2,20));
+		List<Employee> empList = new ArrayList<Employee>();
+	Employee emp1 = new Employee("Emp1",1,10);
+	Employee emp2 = new Employee("Emp2",2,20);
+	Employee emp3 = new Employee("Emp3",3,30);
+	Employee emp4 = new Employee("Emp3",3,30);
 	
-	for(int i =0; i<studList.size();++i ) {
-		System.out.println(studList.get(i).getName());
-	}
+	empList.add(emp1);
+	empList.add(emp2);
+	empList.add(emp3);
+	empList.add(emp4);
 	
+	double avgsal,sum = 0;
+	
+	for(int i =0; i<empList.size();++i ) {
+		System.out.println(empList.get(i).getName());
+		sum = sum + empList.get(i).getSalary();
 	}
+	avgsal = sum/empList.size();
+	 System.out.println(avgsal);
+	
+	 Collection<Employee> coll = empList;
+	 
+	 Iterator<Employee> itr = coll.iterator();
+	
+	 while( itr.hasNext()) {
+		System.out.println(itr.next().getName());
+		
+	}
+	 // empList.remove(emp1);
+	System.out.println(coll.size());
+	
+	Set<Employee> setEmp = new HashSet<Employee>();
+	setEmp.addAll(empList);
+	
+	System.out.println(setEmp.size());
 
+	
+	
+	
+	
+	}
+	
+	
 }
