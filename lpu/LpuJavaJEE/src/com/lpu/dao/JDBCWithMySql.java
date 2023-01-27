7e4bf19e3aa9e0c2efce23b4af4fdcda9d2ea3c4
Gyanendra Singh <gyanendra.singh@congnizant.com>
2023-01-27 18:25:25 +0530

    added close in jdbc

--- lpu/LpuJavaJEE/src/com/lpu/dao/JDBCWithMySql.java
+++ lpu/LpuJavaJEE/src/com/lpu/dao/JDBCWithMySql.java
@@ -5,12 +5,16 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.lpu.domain.Employee;
 
 public class JDBCWithMySql {
-
+	static Connection con;
 	public static void main(String[] args) {
 		try {
-			Connection con =
+			 con =
 					DriverManager.getConnection
 					("jdbc:mysql://localhost:3306/hr", 
 							"root", "full2work");
@@ -18,14 +22,29 @@ public class JDBCWithMySql {
 			Statement stmt = con.createStatement();
 			String sql = "select * from employees";
 			ResultSet rs = stmt.executeQuery(sql);
+			List lstEmp = new LinkedList();
+			
 			while(rs.next()) {
+				String name = rs.getString(2) +" "+ rs.getString(3);
+				Employee emp = new Employee(name,rs.getInt(1),rs.getDouble("salary"));
+				lstEmp.add(emp);
+			    
 				System.out.println(rs.getInt(1)+ " " + rs.getString(2));
+			   System.out.println(lstEmp.size());
+			
 			}
 			
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
-		} 
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+		}
 
 	}
 
