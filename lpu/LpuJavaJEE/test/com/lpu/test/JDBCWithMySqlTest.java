0cbde208c222bb70b797ee5759c3a4bd6004ed1c
Gyanendra Singh <gyanendra.singh@congnizant.com>
2023-01-27 19:06:18 +0530

    added junit

---
+++ lpu/LpuJavaJEE/test/com/lpu/test/JDBCWithMySqlTest.java
@@ -0,0 +1,19 @@
+package com.lpu.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JDBCWithMySqlTest {
+
+	@Test
+	void testCreateStatement() {
+		System.out.println("I am testing");
+	}
+	
+	@Test
+	void testPreparedStatement() {
+		System.out.println("I am testing Preparedstatent");
+	}
+
+}
