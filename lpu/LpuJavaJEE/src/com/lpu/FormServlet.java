ec21d90e349cd94ef053ea30d74b567175a60c02
Gyanendra Singh <gyanendra.singh@congnizant.com>
2023-02-02 12:16:47 +0530

    Added jdbc to servlet

--- lpu/LpuJavaJEE/src/com/lpu/FormServlet.java
+++ lpu/LpuJavaJEE/src/com/lpu/FormServlet.java
@@ -2,12 +2,19 @@ package com.lpu;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 
 /**
  * Servlet implementation class FormServlet
@@ -34,6 +41,40 @@ public class FormServlet extends HttpServlet {
 		String fname = request.getParameter("fname");
 		String lname = request.getParameter("lname");
 		pw.append("Welcome "+fname+" "+lname );
+		Connection con=null;
+		  try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	        } catch (ClassNotFoundException e) {
+	            System.out.println("Class not found " + e);
+	        }
+		try {
+		 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hr", 
+				"root", "full2work");
+		String sql = "Select * from Employees where first_name = ?";
+		 
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, fname);
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()) {
+			pw.append(" "+rs.getInt(1));
+			pw.append(" "+rs.getString(2));
+			pw.append(" "+rs.getString(3));
+			pw.append(" "+rs.getString(4));
+			pw.append(" "+rs.getString(5));
+			
+		}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 	/**
