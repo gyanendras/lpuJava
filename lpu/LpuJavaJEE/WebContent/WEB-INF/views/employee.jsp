f44cbc11d1ef18fc5419cb2dcfc6cb066a188ad2
Gyanendra Singh <gyanendra.singh@congnizant.com>
2023-02-03 12:14:01 +0530

    added table processing in jsp

--- lpu/LpuJavaJEE/WebContent/WEB-INF/views/employee.jsp
+++ lpu/LpuJavaJEE/WebContent/WEB-INF/views/employee.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ page import="java.util.List" %>
+    <%@ page import="com.lpu.domain.Employee" %>
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +10,36 @@
 <title>Insert title here</title>
 </head>
 <body>
+<h1 style="color:red">
+<% out.println("Hello World!");
+List<Employee> ls = (List)request.getAttribute("empList");
+out.println(ls.size());
+for(Employee emp:ls){%>
+
+<table style="border: 2px solid red">
+<tr style="border: 1px solid yellow">
+<td style="border: 1px solid blue">
+<%= emp.getName()%>
+</td>
+<td style="border: 1px solid blue">
+<%= emp.getEmpId()%>
+</td>
+<td style="border: 1px solid blue">
+<%= emp.getSalary()%>
+</td>
+
+</tr>
+
+</table>
+
+<% 
+}
+%>
+
+</h1>
+<h2 style="color:blue">
 <%=new java.util.Date() %>
+</h2>
 
 </body>
 </html>
--- lpu/LpuJavaJEE/src/com/lpu/web/FormServlet.java
+++ lpu/LpuJavaJEE/src/com/lpu/web/FormServlet.java
@@ -1,4 +1,4 @@
-package com.lpu;
+package com.lpu.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,12 +7,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.lpu.domain.Employee;
 
 
 
@@ -37,10 +41,10 @@ public class FormServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter pw =response.getWriter();
-		pw.append("<h1>Hi From Server!</>");
+		//pw.append("<h1>Hi From Server!</>");
 		String fname = request.getParameter("fname");
 		String lname = request.getParameter("lname");
-		pw.append("Welcome "+fname+" "+lname );
+		//pw.append("Welcome "+fname+" "+lname );
 		Connection con=null;
 		  try {
 	            Class.forName("com.mysql.cj.jdbc.Driver");
@@ -50,17 +54,37 @@ public class FormServlet extends HttpServlet {
 		try {
 		 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hr", 
 				"root", "full2work");
-		String sql = "Select * from Employees where first_name = ?";
+		String sql = "Select * from Employees where employee_id between 100 and ?";
 		 
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setString(1, fname);
 		ResultSet rs = ps.executeQuery();
+		List<Employee> ls = new ArrayList();
 		while(rs.next()) {
+			Employee emp = new Employee(rs.getString(2), rs.getInt(1), rs.getDouble(8));
+			ls.add(emp);
+			pw.append("<table style=\"border:2px solid red;\" >");
+			pw.append("<tr>");
+			pw.append("<td style=\"border:2px solid blue;\" >");
 			pw.append(" "+rs.getInt(1));
+			pw.append("</td>");
+			pw.append("<td style=\"border:2px solid blue;\" >");
 			pw.append(" "+rs.getString(2));
+			pw.append("</td>");
+			pw.append("<td style=\"border:2px solid blue;\" >");
 			pw.append(" "+rs.getString(3));
+			pw.append("</td>");
+			pw.append("<td style=\"border:2px solid blue;\" >");
 			pw.append(" "+rs.getString(4));
+			pw.append("</td>");
+			pw.append("<td style=\"border:2px solid blue;\" >");
 			pw.append(" "+rs.getString(5));
+			pw.append("</td>");
+			pw.append("</tr>");
+			pw.append("</table>");
+			
+			request.setAttribute("empList", ls);
+			request.getRequestDispatcher("/emp").forward(request, response);
 			
 		}
 		}catch(SQLException e) {
@@ -82,6 +106,7 @@ public class FormServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//response.getWriter().append("In Post");
 		doGet(request, response);
 	}
 
